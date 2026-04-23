cde9a1d369fcd63d2236977950ae086c3cb7b831
radaiko <a.radlingmayr@icloud.com>
2026-04-23 10:27:03 +0200

    Add QuickAddWindow XAML with title, due-date, parse preview

---
+++ src/Orbital.App/Views/QuickAddWindow.axaml.cs
@@ -0,0 +1,45 @@
+// src/Orbital.App/Views/QuickAddWindow.axaml.cs
+namespace Orbital.App.Views;
+
+using System;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Markup.Xaml;
+using Orbital.Core.ViewModels;
+
+public partial class QuickAddWindow : Window
+{
+    public event Action? SubmitRequested;
+    public event Action? CancelRequested;
+
+    public QuickAddWindow()
+    {
+        InitializeComponent();
+        KeyDown += OnKeyDown;
+        Deactivated += (_, _) => CancelRequested?.Invoke();
+    }
+
+    private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+
+    private void OnKeyDown(object? sender, KeyEventArgs e)
+    {
+        switch (e.Key)
+        {
+            case Key.Escape:
+                e.Handled = true;
+                CancelRequested?.Invoke();
+                break;
+            case Key.Enter:
+                e.Handled = true;
+                SubmitRequested?.Invoke();
+                break;
+        }
+    }
+
+    public void FocusTitle()
+    {
+        var titleBox = this.FindControl<TextBox>("TitleBox");
+        titleBox?.Focus();
+        titleBox?.SelectAll();
+    }
+}
